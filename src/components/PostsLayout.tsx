8a6aaedbfed265f91e69b55a87145e256b38d755
literaryverses <98240549+literaryverses@users.noreply.github.com>
2025-03-01 16:28:44 -0700

    modifying layout for different types of blog posts

--- src/components/PostsLayout.tsx
+++ src/components/PostsLayout.tsx
@@ -6,7 +6,26 @@ type PostsLayoutProps = {
 }
 
 function PostsLayout({title}: PostsLayoutProps) {
-    const posts = myCollections[title];
+    let posts: Post[] = [];
+
+    if (title === 'projects') {
+        posts = myCollections[title];
+    } else {
+        // everything for blog
+        posts = Object.entries(myCollections)
+        .filter(([key]) => key !== 'projects')
+        .map(([key, value]) => 
+            value.map(post => ({
+                ...post,
+                'title': key,
+                'url': `/myWebsite/#/${key}/${post.url}`,
+            }))
+        )
+        .flat();
+    }
+
+    // sort by date to render in chronological order
+    posts.sort((a, b) => a.date.getTime() - b.date.getTime());
 
     return (
         <div id="main">
@@ -19,10 +38,10 @@ function PostsLayout({title}: PostsLayoutProps) {
                     <article key={post.date.toDateString()}>
                         <header>
                             <span className="date">{post.date.toDateString()}</span>
-                            <h2><a href={title === 'projects' ? post.url : `/myWebsite/#/${title}/${post.url}`}>{post.title}</a></h2>
+                            <h2><a href={post.url}></a></h2>
                             <h4>{post.description}</h4>
                         </header>
-                        <a href={title === 'projects' ? post.url : `/myWebsite/#/${title}/${post.url}`}
+                        <a href={post.url}
                         className="image fit"><img src={`/myWebsite/${post.imageUrl}`}/></a>
                     </article>
                 ))}
